b8e71400cafe9f952c6328cd4b93e1f60bd43814
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-08 02:17:08 +0300

    classPhoneContact

---
+++ OOPFirst/Functions.cpp
@@ -0,0 +1,8 @@
+//
+//  Functions.cpp
+//  OOPFirst
+//
+//  Created by Иван Рулевский on 08.04.2024.
+//
+
+#include "Functions.hpp"
---
+++ OOPFirst/Functions.hpp
@@ -0,0 +1,13 @@
+//
+//  Functions.hpp
+//  OOPFirst
+//
+//  Created by Иван Рулевский on 08.04.2024.
+//
+
+#ifndef Functions_hpp
+#define Functions_hpp
+
+#include <stdio.h>
+
+#endif /* Functions_hpp */
